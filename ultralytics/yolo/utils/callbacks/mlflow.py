9d3227d91a73c79d708919c6544ae853dadc8d5f
Yorick Meulenbelt <77830520+YoRigid@users.noreply.github.com>
2023-06-21 23:58:01 +0200

    fixed unset variable error

--- ultralytics/yolo/utils/callbacks/mlflow.py
+++ ultralytics/yolo/utils/callbacks/mlflow.py
@@ -30,6 +30,7 @@ def on_pretrain_routine_end(trainer):
         experiment = mlflow.get_experiment_by_name(experiment_name)
         if experiment is None:
             mlflow.create_experiment(experiment_name)
+            experiment = mlflow.get_experiment_by_name(experiment_name)
         mlflow.set_experiment(experiment_name)
 
         prefix = colorstr('MLFlow: ')
